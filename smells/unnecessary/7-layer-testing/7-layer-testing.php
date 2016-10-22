f822d85023e167a79e4da27a0ce3911ebf1fc42c
Jamie Isaacs <jamie.isaacs@eventfarm.com>
2016-10-22 15:20:52 -0700

    Add transaction and compute test.

--- smells/unnecessary/7-layer-testing/7-layer-testing.php
+++ smells/unnecessary/7-layer-testing/7-layer-testing.php
@@ -5,6 +5,10 @@
  */
 class Financials
 {
+    public function getTransactionProfit(Transaction $transaction): int
+    {
+        return $transaction->getPrice() - $transaction->getCost();
+    }
 }
 
 class SevenLayerTesting extends \PHPUnit_Framework_TestCase
@@ -19,6 +23,32 @@ class SevenLayerTesting extends \PHPUnit_Framework_TestCase
 
     public function testComputesTransactionProfit()
     {
-        $this->assertTrue(true);
+        $transaction = new Transaction(33.22, 20.11);
+
+        $result = $this->subject->getTransactionProfit($transaction);
+
+        $this->assertSame(13, $result);
+    }
+}
+
+class Transaction
+{
+    private $price;
+    private $cost;
+
+    public function __construct(float $price, float $cost)
+    {
+        $this->price = $price;
+        $this->cost = $cost;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getCost(): float
+    {
+        return $this->cost;
     }
 }
